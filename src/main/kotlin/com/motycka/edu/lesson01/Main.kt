b20458030737fd5d546ca3970d8d605df326e737
monikaprotivova <monika.protivova@cleverfarm.ag>
2025-06-09 14:39:27 +0200

    run tests conditionally

--- src/main/kotlin/com/motycka/edu/lesson01/Main.kt
+++ src/main/kotlin/com/motycka/edu/lesson01/Main.kt
@@ -6,5 +6,5 @@ package com.motycka.edu.lesson01
     3. Print the program arguments passed to the main function as: "Program arguments: arg1, arg2, ..."
  */
 fun main(args: Array<String>) {
-
+    // implementation of the main function
 }
